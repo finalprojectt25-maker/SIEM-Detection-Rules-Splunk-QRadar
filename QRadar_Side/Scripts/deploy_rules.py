45ba09406dcffe270409b191af7144b57df4ca04
finalprojectt25-maker <finalprojectt25@gmail.com>
2026-04-04 19:54:50 +0400

    Create deploy_rules.py

---
+++ QRadar_Side/Scripts/deploy_rules.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+# Connection Details
+QRADAR_IP = "13.50.16.252"
+API_TOKEN = "YOUR_TOKEN_HERE" # Bura bayaq aldigin tokeni qoyacaqsan
+
+def deploy_rules_to_siem():
+    print(f"Connecting to QRadar at {QRADAR_IP}...")
+    
+    # Header configuration for API
+    headers = {
+        'SEC': API_TOKEN,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    # In a professional scenario, this script reads files from the /Detection_Rules folder
+    # and sends them to QRadar's REST API endpoint.
+    
+    # Example logic:
+    # response = requests.get(f"https://{QRADAR_IP}/api/analytics/rules", headers=headers, verify=False)
+    # print("Rules successfully synced from GitHub!")
+
+if __name__ == "__main__":
+    deploy_rules_to_siem()
